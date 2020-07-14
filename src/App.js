d2fee285246bc22bda57e3301a53a343e656929c
WMXPY <wm@mengw.io>
2020-07-14 15:59:03 -0500

    add initial content

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (<div>
-    Hello World
+  return (<div className="container">
+    <header className="title">
+      Hello World
+    </header>
+    <section className="content">
+      <p>How are you Indian Mi Fans</p>
+      <p>
+        Do you like&nbsp;
+        <a href="https://www.mi.com/in/mi4i/">Mi4i</a>
+      </p>
+      <p>How are you Indian Mi Fans</p>
+      <p>Do you like Mi Band</p>
+    </section>
+    <footer className="footer">
+      By Myself
+    </footer>
   </div>);
 }
 
